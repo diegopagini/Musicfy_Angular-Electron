2febbfa3ec8ba2f8fb64475219421c7ce97d799f
Diego Pagini <diego.pagini@neoris.com>
2022-08-19 14:53:27 +0200

    dependencies

---
+++ electron/dist/main.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Import necesarios
+var electron_1 = require("electron");
+var path = require("path");
+var url = require("url");
+// Inicializamos la ventana de Electron
+var win;
+function createWindow() {
+    win = new electron_1.BrowserWindow({ width: 800, height: 600 });
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, "/../../dist/musicfy/index.html"),
+        protocol: 'file:',
+        slashes: true,
+    }));
+    win.webContents.openDevTools();
+    win.on('closed', function () {
+        win = null;
+    });
+}
+// Para ver el estado de la app
+electron_1.app.on('ready', createWindow);
+electron_1.app.on('activate', function () {
+    if (win === null) {
+        createWindow();
+    }
+});
+//# sourceMappingURL=main.js.map
